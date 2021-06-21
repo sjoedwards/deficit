4723c5be6b522b93fb873033fe93c6d1d61835df
sjoedwards <samuel.j.o.edwards@gmail.com>
2021-06-21 22:21:24 +0100

    updated tests

--- src/logger/warn.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.logWarning = void 0;
-var logWarning = function (message, ctx) {
-    return console.warn(message, JSON.stringify(ctx.state));
-};
-exports.logWarning = logWarning;
