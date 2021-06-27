9913d73d17f324cd00a37a4820e497c7cca240f3
Sam Edwards <samuel.j.o.edwards@gmail.com>
2021-06-27 20:17:48 +0100

    move to api route (#22)

--- src/middleware/authz.ts
+++ src/middleware/authz.ts
@@ -53,8 +53,17 @@ const authzMiddleware = async (ctx: Context, next: Next) => {
     }
     ctx.cookies.set("accessToken", tokens.access_token, {
       maxAge: tokens.expires_in,
+      domain:
+        process.env.NODE_ENV === "development"
+          ? "localhost"
+          : "deficit-sjoedwards.vercel.app",
     });
-    ctx.cookies.set("refreshToken", tokens.refresh_token);
+    ctx.cookies.set("refreshToken", tokens.refresh_token, {
+      domain:
+        process.env.NODE_ENV === "development"
+          ? "localhost"
+          : "deficit-sjoedwards.vercel.app",
+    });
     const redirectPath = ctx.cookies.get("path") || "/auth";
     ctx.redirect(redirectPath);
   }
