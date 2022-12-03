ec8fb361a3200b4dd8f4cf09621a3226d925dd81
Sam Edwards <samuel.j.o.edwards@gmail.com>
2022-12-03 22:27:07 +0000

    Update index.tsx (#66)
    * Update index.tsx
    
    * updated tests and goal

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>/__tests__/jest.setup.ts"],
+  globalSetup: "<rootDir>/__tests__/jest.global.setup.ts",
   testEnvironment: "jsdom",
   testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
   testPathIgnorePatterns: [
